71500a576c1bb1c1736e238e483e33e02860d095
Hchan2 <seongchan_@naver.com>
2025-11-09 18:44:03 +0900

    feat: add a footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 
 <template>
   <router-view />
+  <footer class="fixed bottom-0 inset-x-0 w-full py-4 text-center text-gray-500 text-xs bg-white z-10">
+    @copyright 2025 오늘의맛기온. All rights reserved. 기상청/에어코리아 공공데이터포털 제공
+  </footer>
 </template>
 
 <style>
