c3a96269cd9c538c152a8adc03bea08f4a9c3203
Hchan2 <seongchan_@naver.com>
2025-11-18 14:41:37 +0900

    refactor: add a footer content

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <router-view />
   <footer class="fixed bottom-0 inset-x-0 w-full py-4 text-center text-gray-500 text-xs bg-white z-10">
-    @copyright 2025 오늘의맛기온. All rights reserved. 기상청/에어코리아 공공데이터포털 제공
+    @copyright 2025 오늘의맛기온. All rights reserved. 기상청/에어코리아/VWOLRD 공공데이터포털 제공
   </footer>
 </template>
 
