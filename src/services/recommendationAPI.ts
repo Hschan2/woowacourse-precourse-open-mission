58ddaeec8998ab562c6bab00a5db8684a1ecad67
Hchan2 <seongchan_@naver.com>
2025-11-23 17:10:52 +0900

    refactor: modify a import url

--- src/services/recommendationAPI.ts
+++ src/services/recommendationAPI.ts
@@ -1,6 +1,6 @@
 import { API_BASE_URL, RECOMMEND_FOOD_API_PATH } from "/src/constants/api";
 import { ERROR_MESSAGES } from "/src/constants/messages";
-import type { Recommendation } from "/src/composables/useFoodRecommendation";
+import type { Recommendation } from "/src/hooks/useFoodRecommendation";
 
 export const callFoodRecommendationAPI = async (
   prompt: string
