e8a2cf7080b18218ddaa26cf9baf51b0b9d9a3a3
Hchan2 <seongchan_@naver.com>
2025-11-24 17:08:00 +0900

    refactor: add a screen setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': '425px',
+        'tablet': '768px',
+      },
+    },
   },
   plugins: [],
 }
