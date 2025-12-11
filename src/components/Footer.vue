e249e8b592a86c7cefcf3c5ae92f5db22639443f
Hchan2 <seongchan_@naver.com>
2025-12-11 16:59:25 +0900

    refactor: create a footer component from App.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer
+    class="fixed bottom-0 inset-x-0 w-full py-2 mobile:py-3 tablet:py-4 text-center text-gray-500 text-[10px] mobile:text-xs bg-white z-10"
+  >
+    @copyright 2025 오늘의맛기온. All rights reserved. 기상청/에어코리아/VWOLRD
+    공공데이터포털 제공
+  </footer>
+</template>
